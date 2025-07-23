1c30a0e6dc4bc62e5331239d278269e376a59523
Ali Abdelrahman <ali.abdalrhman.606@gmail.com>
2025-07-23 22:16:30 +0300

    Load products data by fetch method

--- data/products.js
+++ data/products.js
@@ -60,6 +60,29 @@ class Clothing extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productsData) => {
+      products = productsData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        }
+        return new Product(productDetails);
+      });
+
+      console.log(products);
+    });
+
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log("Next Step");
+});
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -78,6 +101,7 @@ export function loadProducts(fun) {
   xhr.open("GET", "https://supersimplebackend.dev/products");
   xhr.send();
 }
+ */
 
 /*
 export const products = [
